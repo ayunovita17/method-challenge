c742421444b92b92a6493782541a53dcbb7bf077
ayunovita17 <92360606+ayunovita17@users.noreply.github.com>
2021-10-18 11:24:12 +0700

    Add files via upload

---
+++ method.java
@@ -0,0 +1,21 @@
+public class method {
+    
+    public static void main(String[] args) {
+        System.out.println(hasil(12));    
+        myMethod();
+        }
+        
+        static void myMethod(){
+            System.out.println("Ayu Novita, 12");
+
+        }
+        static boolean hasil (int angka){
+            if(angka > 14 ){
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+}
